3d6478b1f95105662010927f9dca0b99a3e2bdeb
Bhanuka-Fernando <bhanukafernando2@gmail.com>
2025-01-13 08:11:44 +0530

    created users data paths using axios

--- Frontend/userform/src/Users.js
+++ Frontend/userform/src/Users.js
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react"
-import axios from "axios"
-
-
+import Axios from "axios"
 
 const Users = () => {
 
@@ -14,6 +12,75 @@ const Users = () => {
     useEffect(() => {
         getUserData();
     }, []);
+
+    const getUserData = () => {
+        Axios.get('')
+            .then((response) => {
+                setUsers(response.data.response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    const addUser = (data) => {
+        setSubmitted(true);
+
+        const payload = {
+            id: data.id,
+            name: data.name,
+        };
+    
+
+        Axios.post('', payload)
+            .then(() => {
+                console.log('success');
+                getUserData();
+                setSubmitted(false);
+                isEdit(false);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };    
+
+    const updateUser = (data) => {
+        setSubmitted(true);
+        const payload = {
+            id:data.id,
+            name: data.name,
+        };
+        Axios.post('', payload)
+            .then(() => {
+                console.log('success');
+                getUserData();
+                setSubmitted(false);
+                isEdit(false);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    const deleteUser = (data) => {
+        setSubmitted(true);
+
+        const payload = {
+            id:data.id,
+            name: data.name,
+        };
+
+        Axios.post('', payload)
+            .then(() => {
+                console.log('success');
+                getUserData();
+                setSubmitted(false);
+                isEdit(false);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
 
     return (
